f1de82e6d4b6c42a484bd737bb3984544a2fb8de
Mohammad Ghazanfar Ali Danish <ghazanfar.danish@iese.fraunhofer.de>
2022-01-27 17:11:46 +0100

    Adds getClientSecret method working
    Signed-off-by: Mohammad Ghazanfar Ali Danish <ghazanfar.danish@iese.fraunhofer.de>

--- basyx.examples/src/main/java/org/eclipse/basyx/examples/scenarios/authorization/KeyCloakProvider.java
+++ basyx.examples/src/main/java/org/eclipse/basyx/examples/scenarios/authorization/KeyCloakProvider.java
@@ -2,10 +2,13 @@ package org.eclipse.basyx.examples.scenarios.authorization;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Arrays;
+import java.util.List;
 
 import org.junit.Test;
 import org.keycloak.admin.client.Keycloak;
 import org.keycloak.admin.client.KeycloakBuilder;
+import org.keycloak.admin.client.resource.ClientResource;
+import org.keycloak.admin.client.resource.ClientsResource;
 import org.keycloak.admin.client.resource.RealmResource;
 import org.keycloak.representations.idm.ClientRepresentation;
 import org.keycloak.representations.idm.CredentialRepresentation;
@@ -16,6 +19,9 @@ public class KeyCloakProvider {
 	private static final String FILE_BASE_PATH = "src/main/resources";
 	private static final String REALM_FILE_NAME = "Test_realm.json";
 	private static final String CLIENT_FILE_NAME = "Test_client.json";
+	
+//	private final RealmRepository realmRepository;
+	
 	@Test
 	public void createRealm() throws IOException {
 		Keycloak keycloak = KeycloakBuilder.builder()
@@ -28,7 +34,9 @@ public class KeyCloakProvider {
 		
 		RealmRepresentation rr = new RealmRepresentation();
 //		createRealmFromJson(keycloak);
-		createClient(keycloak);
+//		createClient(keycloak);
+//		createTestClient(keycloak);
+		getClientId(keycloak);
 //		rr.setId("basyx-demo");
 //		rr.setRealm("basyx-demo");
 //		rr.setDefaultSignatureAlgorithm("RS256");
@@ -39,20 +47,20 @@ public class KeyCloakProvider {
 		
 		
 		
-		CredentialRepresentation credential = new CredentialRepresentation();
-		credential.setType(CredentialRepresentation.PASSWORD);
-		credential.setValue("1234");
-
-		UserRepresentation user = new UserRepresentation();
-		user.setUsername("test");
-		user.setFirstName("test");
-		user.setLastName("test");
-		user.setEmail("test@gmail.com");
-		user.setCredentials(Arrays.asList(credential));
-		user.setEnabled(true);
-		user.setRealmRoles(Arrays.asList("admin"));
-
-		keycloak.realm("test-realm").users().create(user);
+//		CredentialRepresentation credential = new CredentialRepresentation();
+//		credential.setType(CredentialRepresentation.PASSWORD);
+//		credential.setValue("1234");
+//
+//		UserRepresentation user = new UserRepresentation();
+//		user.setUsername("test");
+//		user.setFirstName("test");
+//		user.setLastName("test");
+//		user.setEmail("test@gmail.com");
+//		user.setCredentials(Arrays.asList(credential));
+//		user.setEnabled(true);
+//		user.setRealmRoles(Arrays.asList("admin"));
+//
+//		keycloak.realm("test-realm").users().create(user);
 	}
 	
 	 public void createRealmFromJson(Keycloak keycloak) {
@@ -77,9 +85,52 @@ public class KeyCloakProvider {
 //			clientRepresentation.setClientId(clientId);
 //			clientRepresentation.setProtocol("openid-connect");
 //			clientRepresentation.setSecret(clientId);
+			System.out.println(clientRepresentation.getSecret());
+			System.out.println(clientRepresentation.hashCode());
 			createdRealmResource.clients().create(clientRepresentation);
+//			System.out.println(keycloak.realm("basyx-demo").clients().get("basyx-demo").getSecret().toString());
 
 		    return true;
 		}
+	 
+	 public boolean createTestClient(Keycloak keycloak) throws IOException {
+			RealmResource createdRealmResource = keycloak.realms().realm("basyx-demo");
+			ClientRepresentation clientRepresentation = new ClientRepresentation();
+			clientRepresentation.setClientId("Test-CLIENT");
+			clientRepresentation.setProtocol("openid-connect");
+			clientRepresentation.setClientAuthenticatorType("client-secret");
+			clientRepresentation.setSecret("SECRET-TEST");
+			clientRepresentation.setAuthorizationServicesEnabled(true);
+			createdRealmResource.clients().create(clientRepresentation);
+//			System.out.println(keycloak.realm("basyx-demo").clients().get("basyx-demo").getSecret().toString());
+
+		    return true;
+		}
+	 
+	 public void getClientId(Keycloak keycloak) {
+//		 System.out.println(keycloak.realm("basyx-demo").clients().get("basyx-demo").getSecret().toString());
+		 System.out.println(keycloak.realm("basyx-demo").clients().findByClientId("basyx-demo").toString());
+		 List<ClientRepresentation> clr = keycloak.realm("basyx-demo").clients().findByClientId("basyx-demo");
+		 System.out.println();
+		 ClientsResource CR = keycloak.realm("basyx-demo").clients();
+		 System.out.println(CR.findByClientId("basyx-demo"));
+		 ClientResource cres = keycloak.realm("basyx-demo").clients().get("3d750102-fbe2-40eb-9a24-ca379e7dbfbb");
+		 System.out.println(CR.findAll());
+		 
+		 for(ClientRepresentation cr : CR.findAll()) {
+			 System.out.println(cr.getClientId());
+			 System.out.println(cr.getSecret());
+			 System.out.println(cr.getName());
+			 System.out.println(cr.hashCode());
+			 System.out.println(cr.getId());
+			 System.out.println("$%$%$%$%%$%$%$%$%$%$%$%$%$");
+		 }
+		 
+		 System.out.println(cres.getSecret().getValue());
+		 
+//		 for(ClientRepresentation cr : clr) {
+//			 System.out.println(cr.getSecret());
+//		 }
+	 }
 
 }
