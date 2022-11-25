d54ca1dd0b3d70b0e46d8651e402c7da5a39afe0
Frank Schnicke <77283144+FrankSchnicke@users.noreply.github.com>
2022-11-25 16:28:07 +0100

    Integrates MQTT into hello world project (#49)
    * Integrates MQTT into hello world project
    
    * Update StaticDynamicScenario.java
    
    * Update RegistryContext.properties

--- basyx.hello_world/src/main/java/org/eclipse/basyx/hello_world/HelloWorldServer.java
+++ basyx.hello_world/src/main/java/org/eclipse/basyx/hello_world/HelloWorldServer.java
@@ -24,6 +24,8 @@
  ******************************************************************************/
 package org.eclipse.basyx.hello_world;
 
+import java.io.IOException;
+
 import org.eclipse.basyx.aas.manager.ConnectedAssetAdministrationShellManager;
 import org.eclipse.basyx.aas.metamodel.api.parts.asset.AssetKind;
 import org.eclipse.basyx.aas.metamodel.map.AssetAdministrationShell;
@@ -31,7 +33,6 @@ import org.eclipse.basyx.aas.metamodel.map.descriptor.CustomId;
 import org.eclipse.basyx.aas.metamodel.map.parts.Asset;
 import org.eclipse.basyx.aas.registration.proxy.AASRegistryProxy;
 import org.eclipse.basyx.components.aas.AASServerComponent;
-import org.eclipse.basyx.components.aas.configuration.AASServerBackend;
 import org.eclipse.basyx.components.aas.configuration.BaSyxAASServerConfiguration;
 import org.eclipse.basyx.components.configuration.BaSyxContextConfiguration;
 import org.eclipse.basyx.components.registry.RegistryComponent;
@@ -41,17 +42,25 @@ import org.eclipse.basyx.submodel.metamodel.api.identifier.IIdentifier;
 import org.eclipse.basyx.submodel.metamodel.map.Submodel;
 import org.eclipse.basyx.submodel.metamodel.map.submodelelement.dataelement.property.Property;
 
+import io.moquette.broker.Server;
+import io.moquette.broker.config.ClasspathResourceLoader;
+import io.moquette.broker.config.IConfig;
+import io.moquette.broker.config.IResourceLoader;
+import io.moquette.broker.config.ResourceLoaderConfig;
+
 /**
  * This class starts an AAS server and a Registry server
  * 
  * An AAS and a Submodel containing a Property "maxTemp"
  * is uploaded to the AAS server and registered in the Registry server.
  * 
+ * Additionally, MQTT eventing is configured and an MQTT broker is started at tcp://localhost:1884
+ * 
  * @author schnicke, conradi
  *
  */
-public class Server {
-	// Server URLs
+public class HelloWorldServer {
+	// HelloWorldServer URLs
 	public static final String REGISTRYPATH = "http://localhost:4000/registry";
 	public static final String AASSERVERPATH = "http://localhost:4001/aasServer";
 
@@ -59,23 +68,23 @@ public class Server {
 	public static final IIdentifier OVENAASID = new CustomId("eclipse.basyx.aas.oven");
 	public static final IIdentifier DOCUSMID = new CustomId("eclipse.basyx.submodel.documentation");
 	public static final String MAXTEMPID = "maxTemp";
+	private static Server mqttBroker;
 
-	public static void main(String[] args) {
-		// Create Infrastructure
-		startRegistry();
+	public static void main(String[] args) throws IOException {
+		startMqttBroker();
+		startAASRegistry();
 		startAASServer();
 
 		// Create Manager - This manager is used to interact with an AAS server
-		ConnectedAssetAdministrationShellManager manager = 
-				new ConnectedAssetAdministrationShellManager(new AASRegistryProxy(REGISTRYPATH));
-		
+		ConnectedAssetAdministrationShellManager manager = new ConnectedAssetAdministrationShellManager(new AASRegistryProxy(REGISTRYPATH));
+
 		// Create AAS and push it to server
 		Asset asset = new Asset("ovenAsset", new CustomId("eclipse.basyx.asset.oven"), AssetKind.INSTANCE);
 		AssetAdministrationShell shell = new AssetAdministrationShell("oven", OVENAASID, asset);
-		
+
 		// The manager uploads the AAS and registers it in the Registry server
 		manager.createAAS(shell, AASSERVERPATH);
-		
+
 		// Create submodel
 		Submodel documentationSubmodel = new Submodel("documentationSm", DOCUSMID);
 
@@ -92,26 +101,44 @@ public class Server {
 	/**
 	 * Starts an empty registry at "http://localhost:4000"
 	 */
-	private static void startRegistry() {
+	private static void startAASRegistry() {
+		RegistryComponent registry = configureAASRegistry();
+
+		registry.startComponent();
+	}
+
+	private static RegistryComponent configureAASRegistry() {
 		BaSyxContextConfiguration contextConfig = new BaSyxContextConfiguration(4000, "/registry");
 		contextConfig.setAccessControlAllowOrigin("*");
+		
 		BaSyxRegistryConfiguration registryConfig = new BaSyxRegistryConfiguration(RegistryBackend.INMEMORY);
-		RegistryComponent registry = new RegistryComponent(contextConfig, registryConfig);
-
-		// Start the created server
-		registry.startComponent();
+		
+		return new RegistryComponent(contextConfig, registryConfig);
 	}
 
 	/**
 	 * Startup an empty server at "http://localhost:4001/"
 	 */
 	private static void startAASServer() {
+		AASServerComponent aasServer = configureAASServer();
+
+		aasServer.startComponent();
+	}
+
+	private static AASServerComponent configureAASServer() {
 		BaSyxContextConfiguration contextConfig = new BaSyxContextConfiguration(4001, "/aasServer");
 		contextConfig.setAccessControlAllowOrigin("*");
-		BaSyxAASServerConfiguration aasServerConfig = new BaSyxAASServerConfiguration(AASServerBackend.INMEMORY, "", REGISTRYPATH);
-		AASServerComponent aasServer = new AASServerComponent(contextConfig, aasServerConfig);
 
-		// Start the created server
-		aasServer.startComponent();
+		BaSyxAASServerConfiguration aasServerConfig = new BaSyxAASServerConfiguration();
+		aasServerConfig.loadFromDefaultSource();
+
+		return new AASServerComponent(contextConfig, aasServerConfig);
+	}
+
+	protected static void startMqttBroker() throws IOException {
+		mqttBroker = new Server();
+		IResourceLoader classpathLoader = new ClasspathResourceLoader();
+		final IConfig classPathConfig = new ResourceLoaderConfig(classpathLoader);
+		mqttBroker.startServer(classPathConfig);
 	}
 }
